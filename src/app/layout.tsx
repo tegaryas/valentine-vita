929b0687c2f1b53a79f6ecffe539dd5cea66139b
tegaryas <tegar.yas26@gmail.com>
2024-02-14 02:18:01 +0700

    fix: sound play

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,9 +19,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <link rel="shortcut icon" href="../assets/favicon.png" />
+      <link rel="shortcut icon" href="/assets/favicon.png" />
       <body className={press_start_2P.className}>
-        <audio id="audio" src="/assets/music/background-music.mp3" />
+        <audio
+          id="audio"
+          src="https://github.com/tegaryas/valentine-website/blob/main/public/assets/music/background-music.mp3?raw=true"
+        />
         {children}
       </body>
     </html>
